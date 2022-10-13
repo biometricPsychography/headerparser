d3d65662b91cf53c130a2201512fa247d6c5ea6a
biometricPsychography <cyclinglinguist@gmail.com>
2022-10-12 21:03:53 -0500

    Add freeCodeCamp's required features

--- index.js
+++ index.js
@@ -20,11 +20,16 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+    res.json({
+        ipaddress: req.ip,
+        language: req.headers["accept-language"],
+        software: req.headers["user-agent"]
+    });
 });
 
+const PORT = 8080;
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
